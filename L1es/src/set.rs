5b69eed89acdaa183abdaa2c9cdc33a35f99904b
Robert <rnb2135@columbia.edu>
2025-06-13 16:12:34 -0400

    finished set.rs

--- L1es/src/set.rs
+++ L1es/src/set.rs
@@ -37,18 +37,67 @@ impl CacheSet{
     // evicted_tag = Some(tag) if a line was evicted, None otherwise
     pub fn access(&mut self, tag: u64, timestamp: u64) -> (bool, Option<u64>){
 
+        self.access_count += 1;
+
+        for line in &mut self.lines {
+
+            if line.is_hit(tag) {
+
+                line.update(tag, timestamp);
+                self.hit_count += 1;
+                return (true, None);
+            }
+        }
+        self.miss_count += 1;
+        let evicted_tag = self.replace_line(tag, timestamp);
+        (false, evicted_tag)
 
     }
 
     //replace lien by doing LRU policy, returns evicted tag if any 
     fn replace_line(&mut self, tag: u64, timestamp: u64) -> Option<u64> {
 
+        for line in &mut self.lines {
+
+            if !line.valid {
+                line.update(tag, timestamp);
+                return None;
+            }
+        }
+
+        // All the lines are valid:
+
+        let mut lru_idx = 0;
+
+        let mut oldest_time = self.lines[0].last_access;
+
+        for (i, line) in self.lines.iter().enumerate() {
+
+            if line.last_access < oldest_time {
+
+                oldest_time = line.last_access;
+                lru_idx = i;
+            }
+        }
+
+        let evicted_tag = self.lines[lru_idx].tag;
+
+        self.lines[lru_idx].update(tag, timestamp);
+        Some(evicted_tag)
 
     }
 
     // flush a specific tag from this set, this is for flush+reload attacks
     pub fn flush(&mut self, tag: u64) -> bool {
 
+        for line in &mut self.lines {
+
+            if line.is_hit(tag) {
+                line.invalidate();
+                return true;
+            }
+        }
+        false
 
     }
 
