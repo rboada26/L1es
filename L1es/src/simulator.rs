247068b86b09b2a3cef6455eecfad95e6f2674b6
Robert <rnb2135@columbia.edu>
2025-06-18 13:19:43 -0400

    started writing basic tests for simulator.rs

---
+++ L1es/src/simulator.rs
@@ -0,0 +1,27 @@
+use crate::cache::Cache;
+
+pub fn run_basic_test(){
+
+
+}
+
+fn test_basic_pattern(cache: &mut Cache){
+
+
+}
+
+
+fn test_set_conflicts(cache: &mut Cache){
+
+
+}
+
+fn test_flush(cache: &mut Cache){
+
+
+}
+
+fn main(){
+
+    run_basic_test();
+}
