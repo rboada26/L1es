e98956567d2c8df686e793e0c3d53f776ba13e68
Robert <rnb2135@columbia.edu>
2025-06-18 20:52:51 -0400

    start off with prime probe attck

---
+++ L1es/attacks/prime_probe.rs
@@ -0,0 +1,13 @@
+use crate::cache::Cache;
+
+pub struct PrimeProbeAttack {
+
+    // TODO target and eviction sets and probe times vector 
+
+}
+
+impl PrimeProbeAttack {
+
+// TODO new(), gen_eviction_set(), prime(), probe(), sim_victim access(), demo_prime_probe()
+
+}
