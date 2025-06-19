8666af6682a1a24f38f0824c40303a24b601e54a
Robert <rnb2135@columbia.edu>
2025-06-19 15:23:22 -0400

    todo: flush+reload attack

---
+++ L1es/src/attacks/flush_reload.rs
@@ -0,0 +1,15 @@
+use crate::cache::Cache;
+
+pub struct FlushReloadAttack {
+
+    pub monitored_addresses: Vec<u64>,
+    pub acess_times: Vec<u64>,
+    pub detection_results: Vec<bool>,
+}
+
+impl FlushReloadAttack {
+
+    // TODO: new(), flush(), reload(), get_summary(), sim_victim_scenario(), demo_flush_reload()
+
+
+}
